de9edf877f9ee2568f5ea3ce7a5961566a768844
thu <mingfei.net@gmail.com>
2016-12-04 15:27:36 +0800

    Update

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -2,11 +2,9 @@ package controller;
 
 import model.User;
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import util.MyBatisSession;
-
-import java.util.List;
 
 /**
  * Created by Administrator on 2016/12/4.
@@ -15,26 +13,25 @@ import java.util.List;
 @RequestMapping("user")
 public class UserController extends BaseController {
 
+    @Autowired
+    private SqlSession sqlSession;
+
     @RequestMapping("create")
     private String create(User user) {
-        try (SqlSession sqlSession = MyBatisSession.getSqlSession(true)) {
-            sqlSession.insert("user.create", user);
-        }
+        sqlSession.insert("user.create", user);
         return "redirect:/index.jsp";
     }
 
     @RequestMapping("login")
     private String login(User user) {
-        try (SqlSession sqlSession = MyBatisSession.getSqlSession(false)) {
-            user = sqlSession.selectOne("user.login", user);
-            if (user != null) {
-                user.setPassword(null);
-                session.setAttribute("user", user);
-                return "redirect:/home.jsp";
-            } else {
-                request.setAttribute("message", "invalid username or password.");
-                return "/index.jsp";
-            }
+        user = sqlSession.selectOne("user.login", user);
+        if (user != null) {
+            user.setPassword(null);
+            session.setAttribute("user", user);
+            return "redirect:/home.jsp";
+        } else {
+            request.setAttribute("message", "invalid username or password.");
+            return "/index.jsp";
         }
     }
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="${ctx}/static/css/style.css">
 </head>
 <body>
+<h1>index page</h1>
 <form action="${ctx}/user/login" method="post">
     <input type="text" name="username" placeholder="USERNAME"><br>
     <input type="password" name="password" placeholder="PASSWORD"><br>
